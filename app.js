6beced42ce8a860e29549e910ce467dd4cc1122c
Jon Gottfried <jonmarkgo@gmail.com>
2013-10-17 14:38:38 -0400

    Remove some cruft

--- app.js
+++ app.js
@@ -9,24 +9,37 @@ var express = require('express')
 server.listen(3000);
 
 app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
 
 app.get('/', function(req, res) {
-  res.render('index.ejs');
+  if(req.session.clientName) {
+    res.redirect('/client');
+  }
+  else {
+    res.render('index.ejs');
+  }
 });
 
-app.post('/client', function(req, res) {
+function clientRoute(req, res) {
   var capability = new twilio.Capability(
       process.env.TWILIO_ACCOUNT_SID,
       process.env.TWILIO_AUTH_TOKEN
   );
-
-  capability.allowClientIncoming(req.body.name);
+  if(!req.session.clientName) {
+    req.session.clientName = req.body.name;
+  }
+  capability.allowClientIncoming(req.session.clientName);
   capability.allowClientOutgoing(process.env.TWILIO_APP_SID);
   res.render('client.ejs', {
       token: capability.generate(),
       phone_number: process.env.TWILIO_PHONE_NUMBER
   });
-});
+}
+
+app.post('/client', clientRoute);
+app.get('/client', clientRoute);
+
 io.sockets.on('connection', function(socket) {
   console.log('socket.io connected');
   socket.on('incoming', function(caller) {
@@ -46,44 +59,9 @@ app.post('/incoming', function(req, res) {
   var resp = new twilio.TwimlResponse();
 
   resp.dial(function() {
-    this.client('screenpopdemo');
+    this.client(req.body.clientName);
   });
   res.set('Content-Type', 'text/xml');
   res.send(resp.toString());
 
 });
-// io.sockets.on('connection', function(socket) {
-//   console.log('socket.io connected');
-//   socket.on('register', function(data) {
-//     var code = speakeasy.totp({key: 'abc123'});
-//     users.get(data.phone_number, function (geterr, doc, key) {
-//       if (geterr) {
-//         createUser(data.phone_number, code, socket);
-//       }
-//       else if (checkVerified(socket, doc.verified, data.phone_number) == false) {
-//         socket.emit('update', {message: "You have already requested a verification code for that number!"});
-//         socket.emit('code_generated');
-//       }
-//     });
-
-//   });
-
-//   socket.on('verify', function(data) {
-//     users.get(data.phone_number, function (geterr, doc, key) {
-//       if (geterr) {
-//         socket.emit('reset');
-//         socket.emit('update', {message: "You have not requested a verification code for " + data.phone_number + " yet!"});
-//       }
-//       else if (checkVerified(socket, doc.verified, data.phone_number) == false && doc.code == parseInt(data.code)) {
-//         socket.emit('verified');
-//         socket.emit('update', {message: "You have successfully verified " + data.phone_number + "!"});
-//         users.save(data.phone_number, {code: parseInt(data.code), verified: true}, function (saverr) { if (saverr) { throw saverr; }});
-//       }
-//       else {
-//         socket.emit('update', {message: "Invalid verification code!"});
-//       }
-//     });
-
-//   });
-// });
-
